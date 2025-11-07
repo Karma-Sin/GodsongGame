dea3e0ea3a519a07e2ab0840d174bcd887502f34
Karma-Sin <raphaelboue007@gmail.com>
2025-11-07 00:07:14 -0500

    Update CardLibrary.cs
    revamp2---UT

--- Godsong/CardLibrary.cs
+++ Godsong/CardLibrary.cs
@@ -11,6 +11,9 @@ namespace Godsong
         {
             AddHumanCard();
             AddGoblinCard();
+            AddTemplateCard1();
+            AddTemplateCard2();
+            AddTemplateCard3();
         }
 
         private static void AddHumanCard()
@@ -45,6 +48,60 @@ namespace Godsong
             ).CreateCard());
         }
 
+        // ------------------------------
+        // Blank templates ready to fill
+        // ------------------------------
+        private static void AddTemplateCard1()
+        {
+            AllCards.Add(new CardTemplate(
+                "Template1",
+                "Description for Template1",
+                0, 0, 0,
+                new List<Skill>
+                {
+                    SkillLIbrary.HumanStrike,   // Replace with desired skill
+                    SkillLIbrary.HumanLunge,    // Replace with desired skill
+                    SkillLIbrary.HumanLaststand,// Replace with desired skill
+                    SkillLIbrary.HumanCounter   // Replace with desired skill
+                }
+            ).CreateCard());
+        }
+
+        private static void AddTemplateCard2()
+        {
+            AllCards.Add(new CardTemplate(
+                "Template2",
+                "Description for Template2",
+                0, 0, 0,
+                new List<Skill>
+                {
+                    SkillLIbrary.GoblinSuckerPunch,
+                    SkillLIbrary.GoblinLanceBarrage,
+                    SkillLIbrary.GoblinTinkererArmor,
+                    SkillLIbrary.GoblinOilFlare
+                }
+            ).CreateCard());
+        }
+
+        private static void AddTemplateCard3()
+        {
+            AllCards.Add(new CardTemplate(
+                "Template3",
+                "Description for Template3",
+                0, 0, 0,
+                new List<Skill>
+                {
+                    SkillLIbrary.HumanStrike, // Replace with desired skill
+                    SkillLIbrary.GoblinOilFlare, // Replace with desired skill
+                    SkillLIbrary.HumanLunge, // Replace with desired skill
+                    SkillLIbrary.GoblinTinkererArmor // Replace with desired skill
+                }
+            ).CreateCard());
+        }
+
+        // -------------------------
+        // Optional: Get card by name
+        // -------------------------
         public static Card? GetCard(string name)
         {
             return AllCards.Find(c => c.Name == name);
